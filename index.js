aa4fb84ee2987c55ff2535d7a94e4cdf95b1c430
Muhammad Ibrahim <muhdibee@gmail.com>
2023-03-01 15:16:15 +0100

    dispalying date every second using cron job.

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const schedule = require('node-schedule');
 
 const job = schedule.scheduleJob('* * * * *', function() {
     console.log("My first cron job.");
-    // console.log("The time is: ", Date.now().toLocaleString())
+    console.log("The time is: ", new Date().toGMTString())
 })
